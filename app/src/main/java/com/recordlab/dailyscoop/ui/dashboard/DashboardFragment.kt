040c3df9a4383ebceeaeb778dc103498e4e7d512
adj5672 <adj5672@naver.com>
2021-11-01 14:45:44 +0900

    :sparkles: dongjun : select Date in dashboard

--- app/src/main/java/com/recordlab/dailyscoop/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/recordlab/dailyscoop/ui/dashboard/DashboardFragment.kt
@@ -1,14 +1,19 @@
 package com.recordlab.dailyscoop.ui.dashboard
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.NumberPicker
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.recordlab.dailyscoop.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class DashboardFragment : Fragment() {
 
@@ -26,6 +31,61 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val selectedDate : TextView = root.findViewById(R.id.tv_nav_gallery_date)
+        var nowYear = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy"))
+        var nowMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("MM"))
+        selectedDate.text = "$nowYear.$nowMonth"
+
+        val datePickBtn = root.findViewById<View>(R.id.iv_nav_gallery_calender)
+        datePickBtn.setOnClickListener {
+
+            val dialog = AlertDialog.Builder(context).create()
+            val edialog : LayoutInflater = LayoutInflater.from(context)
+            val mView : View = edialog.inflate(R.layout.custom_dialog_datepicker, null)
+
+            val year : com.shawnlin.numberpicker.NumberPicker = mView.findViewById(R.id.np_datepicker_year_picker)
+            val month : com.shawnlin.numberpicker.NumberPicker = mView.findViewById(R.id.np_datepicker_month_picker)
+            val cancel : Button = mView.findViewById(R.id.btn_datepicker_dlg_cancel)
+            val save : Button = mView.findViewById(R.id.btn_datepicker_dlg_ok)
+
+            // 순환 안되게 막기
+            year.wrapSelectorWheel = false
+            month.wrapSelectorWheel = false
+
+            // editText 설정 해제
+            year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+
+            // 최솟값, 최댓값 설정
+            year.minValue = 2015
+            year.maxValue = 2022
+            month.minValue = 1
+            month.maxValue = 12
+
+            year.value = nowYear.toInt()
+            month.value = nowMonth.toInt()
+
+            // 취소 버튼 클릭시
+            cancel.setOnClickListener {
+                dialog.dismiss()
+                dialog.cancel()
+            }
+
+            // 완료 버튼 클릭시
+            save.setOnClickListener {
+                nowYear = year.value.toString()
+                nowMonth = if (month.value < 10) "0${month.value}" else month.value.toString()
+                selectedDate.text = "$nowYear.$nowMonth"
+                dialog.dismiss()
+                dialog.cancel()
+            }
+
+            dialog.setView(mView)
+            dialog.create()
+            dialog.show()
+        }
+
         return root
     }
 }
