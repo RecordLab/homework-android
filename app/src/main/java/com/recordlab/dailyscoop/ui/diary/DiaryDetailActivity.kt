220bc496492579331cf1c88a7a8f2eb43689eed8
WoosubLeee <zbxv1423@naver.com>
2021-11-02 15:53:40 +0900

    :lipstick: woosub create diary detail 화면 작업중

---
+++ app/src/main/java/com/recordlab/dailyscoop/ui/diary/DiaryDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.recordlab.dailyscoop.ui.diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.recordlab.dailyscoop.R
+
+class DiaryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_detail)
+    }
+}
