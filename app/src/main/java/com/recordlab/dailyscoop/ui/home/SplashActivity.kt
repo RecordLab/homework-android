b813dbeced3d4cf4449af90e498281a5d099515c
WoosubLeee <zbxv1423@naver.com>
2021-11-03 14:35:57 +0900

    :construction: woosub splash 로그인/메인 분기 임시 연결

--- app/src/main/java/com/recordlab/dailyscoop/ui/home/SplashActivity.kt
+++ app/src/main/java/com/recordlab/dailyscoop/ui/home/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.recordlab.dailyscoop.MainActivity
 import com.recordlab.dailyscoop.R
+import com.recordlab.dailyscoop.ui.auth.SignUpActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -14,9 +15,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000)
+        // 로그인 되어있지 않을 시, SignUpActivity로
+        // 임시로 t/f 집어넣어 놓음
+        if (false) {
+            Handler().postDelayed({
+                startActivity(Intent(this, SignUpActivity::class.java))
+                finish()
+            }, 3000)
+        // 로그인 되어있을 시, MainActivity로
+        } else {
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
     }
 }
