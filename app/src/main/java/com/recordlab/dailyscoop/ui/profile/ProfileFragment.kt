20a5673f72e96ab64b5b4744bfccff53d23cc2ce
김학영 <gkrdud5698@naver.com>
2021-11-04 16:46:29 +0900

    :poop: hakyoung : help!

--- app/src/main/java/com/recordlab/dailyscoop/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/recordlab/dailyscoop/ui/profile/ProfileFragment.kt
@@ -1,20 +1,43 @@
 package com.recordlab.dailyscoop.ui.profile
 
+import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.recordlab.dailyscoop.R
+import com.recordlab.dailyscoop.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
+    private val REQ_STORAGE_PERMISSION = 9
+    private val REQ_GALLERY = 10
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if(it.resultCode == Activity.RESULT_OK){
+//            it.data?.data?.let{ uri ->
+//                binding.profile.setImageURI(uri)
+//            }
+            binding.profile.setImageURI(it.data?.data)
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +45,7 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 //        val profileViewModel : ProfileViewModel by viewModels()
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
         val textView: TextView = root.findViewById(R.id.tv_profile_test)
@@ -32,7 +56,17 @@ class ProfileFragment : Fragment() {
         // 프로필 변경(카메라 아이콘) 버튼 클릭
         val profileImageBtnClicked = root.findViewById<View>(R.id.profile2)
         profileImageBtnClicked.setOnClickListener {
+            val readPermission = ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
 
+            if(readPermission == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQ_STORAGE_PERMISSION)
+            }else{
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                intent.type = "image/*"
+                //this.startActivityForResult(intent, REQ_GALLERY)
+                startForResult.launch(intent)
+            }
         }
 
         val friendBtnClicked = root.findViewById<View>(R.id.bg1)
@@ -79,5 +113,10 @@ class ProfileFragment : Fragment() {
         return root
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
