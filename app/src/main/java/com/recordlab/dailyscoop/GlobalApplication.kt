4a3f94306fabef1fcd2454383511ed4bd4752a28
김학영 <gkrdud5698@naver.com>
2021-11-18 11:28:52 +0900

    :construction: hakyoung : Add kakao-login

---
+++ app/src/main/java/com/recordlab/dailyscoop/GlobalApplication.kt
@@ -0,0 +1,19 @@
+package com.recordlab.dailyscoop
+
+import android.app.Application
+import android.util.Log
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "ba4c4a64a67509ca547ba2d761e0a8ff")
+
+//        val keyHash = Utility.getKeyHash(this)
+//        Log.d("해시 >> ", keyHash)
+    }
+}
--- app/src/main/java/com/recordlab/dailyscoop/ui/auth/SignInActivity.kt
+++ app/src/main/java/com/recordlab/dailyscoop/ui/auth/SignInActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.kakao.sdk.auth.model.Prompt
+import com.kakao.sdk.user.UserApiClient
 import com.recordlab.dailyscoop.MainActivity
 import com.recordlab.dailyscoop.databinding.ActivitySignInBinding
 import com.recordlab.dailyscoop.network.RetrofitClient.service
@@ -53,18 +55,40 @@ class SignInActivity : AppCompatActivity() {
         }
 
         // 카카오로그인 버튼 클릭
-        val socialKBtnClicked = binding.kakaoBtn
-        socialKBtnClicked.setOnClickListener{
-            Toast.makeText(this.getApplicationContext(),"kakao", Toast.LENGTH_SHORT).show();
-
-        }
+//        val socialKBtnClicked = binding.kakaoBtn
+//        socialKBtnClicked.setOnClickListener{
+//            UserApiClient.instance.loginWithKakaoAccount(this, prompts = listOf(Prompt.LOGIN)) { token, error ->
+//                if (error != null) {
+//                    Toast.makeText(this.applicationContext,"다시 시도해 주세요", Toast.LENGTH_SHORT).show();
+//                }
+//                else if (token != null) {
+//                    Log.i("kakao", "로그인 성공 ${token.accessToken}")
+//
+//                    // 카카오 토큰 서버로 전송
+//
+//                    // 사용자 정보 요청 (기본)
+//                    UserApiClient.instance.me { user, errorr ->
+//                        if (errorr != null) {
+//                            Log.e("kakao", "사용자 정보 요청 실패", errorr)
+//                        }
+//                        else if (user != null) {
+//                            Log.i("kakao", "사용자 정보 요청 성공" +
+//                                    "\n회원번호: ${user.id}" +
+//                                    "\n이메일: ${user.kakaoAccount?.email}" +
+//                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+//                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+//                        }
+//                    }
+//                }
+//            }
+//        }
 
         // 구글 로그인 버튼 클릭
-        val googleBtnClicked = binding.googleBtn
-        googleBtnClicked.setOnClickListener{
-            Toast.makeText(this.getApplicationContext(),"google", Toast.LENGTH_SHORT).show();
-
-        }
+//        val googleBtnClicked = binding.googleBtn
+//        googleBtnClicked.setOnClickListener{
+//            Toast.makeText(this.getApplicationContext(),"google", Toast.LENGTH_SHORT).show();
+//
+//        }
     }
 
     private fun signIn(data: RequestSignIn) {
@@ -81,8 +105,6 @@ class SignInActivity : AppCompatActivity() {
                         edit.putString("token", "Bearer ".plus(to)) // key, value
                         edit.putString("nickname", nic)
                         edit.apply() // 저장 완료
-
-                        //Log.d("token", "로그인 응답 $to")
 
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
