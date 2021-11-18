120f64bdddfce4da8a5aa69aeeedeab83b1222b6
adj5672 <adj5672@naver.com>
2021-11-18 15:01:19 +0900

    :sparkles: 동준 : add quotation bookmark & activity

--- app/src/main/java/com/recordlab/dailyscoop/ui/home/widget/QuotationFragment.kt
+++ app/src/main/java/com/recordlab/dailyscoop/ui/home/widget/QuotationFragment.kt
@@ -1,5 +1,7 @@
 package com.recordlab.dailyscoop.ui.home.widget
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
@@ -11,11 +13,14 @@ import androidx.fragment.app.Fragment
 import com.recordlab.dailyscoop.databinding.ItemMainWidgetQuotationBinding
 import com.recordlab.dailyscoop.network.RetrofitClient.service
 import com.recordlab.dailyscoop.network.enqueue
+import com.recordlab.dailyscoop.network.request.RequestQuotation
 import com.recordlab.dailyscoop.ui.dashboard.DialogYearMonth
 
 class QuotationFragment : Fragment(), View.OnClickListener {
     private var _binding: ItemMainWidgetQuotationBinding? = null
     private val binding get() = _binding!!
+    private lateinit var sharedPref: SharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,11 +29,12 @@ class QuotationFragment : Fragment(), View.OnClickListener {
         _binding = ItemMainWidgetQuotationBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        sharedPref = requireActivity().getSharedPreferences("TOKEN", Context.MODE_PRIVATE)
+
         val quotation = binding.tvMainWidgetQuotation
         quotation.setMovementMethod(ScrollingMovementMethod())
         service.requestQuotation("guest").enqueue(
             onSuccess = {
-//                Log.d("asd", it.body()!![1].respond)
                 quotation.text = it.body()!![1].respond.toString()
             },
             onError = {
@@ -41,12 +47,14 @@ class QuotationFragment : Fragment(), View.OnClickListener {
 
         val bookmarkBtn = binding.ivWidgetQuotationBookmark
         bookmarkBtn.setOnClickListener {
+            val data = RequestQuotation(quotation.text.toString())
+
+            if (!bookmarkBtn.isSelected) {
+                addQuotation(data)
+            } else {
+                delQuotation(data)
+            }
             bookmarkBtn.isSelected = !bookmarkBtn.isSelected
-//            if (!bookmarkBtn.isSelected) {
-//
-//            } else {
-//
-//            }
         }
 
         return view
@@ -57,6 +65,44 @@ class QuotationFragment : Fragment(), View.OnClickListener {
         when (v?.id) {
 
         }
+    }
+
+    private fun addQuotation(data: RequestQuotation) {
+        val header = mutableMapOf<String, String?>()
+
+        header["Content-type"] = "application/json; charset=UTF-8"
+        header["Authorization"] = sharedPref.getString("token", "token")
+
+        service.requestAddQuotation(header = header, quote = data).enqueue(
+            onSuccess = {
+                Log.d("message", it.body()!!.message)
+            },
+            onError = {
+
+            },
+            onFail = {
+
+            }
+        )
+    }
+
+    private fun delQuotation(data: RequestQuotation) {
+        val header = mutableMapOf<String, String?>()
+
+        header["Content-type"] = "application/json; charset=UTF-8"
+        header["Authorization"] = sharedPref.getString("token", "token")
+
+        service.requestDelQuotation(header = header, quote = data).enqueue(
+            onSuccess = {
+                Log.d("message", it.body()!!.message)
+            },
+            onError = {
+
+            },
+            onFail = {
+
+            }
+        )
     }
 
 }
---
+++ app/src/main/java/com/recordlab/dailyscoop/ui/profile/ProfileBookmarkAdapter.kt
@@ -0,0 +1,47 @@
+package com.recordlab.dailyscoop.ui.profile
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.compose.ui.text.font.Typeface
+import androidx.recyclerview.widget.RecyclerView
+import com.recordlab.dailyscoop.R
+import com.recordlab.dailyscoop.network.response.Favorite
+
+class ProfileBookmarkAdapter(private val items: List<Favorite>) :
+    RecyclerView.Adapter<ProfileBookmarkAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflatedView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_bookmark_list, parent, false)
+        return ViewHolder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: Favorite) {
+            val quote = view.findViewById<TextView>(R.id.tv_bookmark_item)
+            val writer = view.findViewById<TextView>(R.id.tv_bookmark_writer)
+            var index = item.quote!!.indexOf("-")
+            if (index == -1) {
+               index = item.quote!!.indexOf("–")
+            }
+
+            quote.text = item.quote!!.substring(0, index)
+            writer.text = "–" + item.quote!!.substring(index+1)
+            quote.setTypeface(null, android.graphics.Typeface.BOLD)
+            writer.setTypeface(null, android.graphics.Typeface.ITALIC)
+
+            itemView.setOnClickListener {
+                Log.d("click", item.quote.toString())
+            }
+        }
+    }
+}
